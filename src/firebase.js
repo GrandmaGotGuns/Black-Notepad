f4091f0b650d9198e79d3d75b6dd8150aa92d0a7
Somendra Saini <somendra2406@outlook.com>
2025-05-11 06:39:24 +0530

    Update firebase.js
    Removed Secrets

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCQ6fTUAHkr0rfc_1HLiqso3wycPiN6h50",
+  apiKey: "secret",
   authDomain: "the-notepad-2e341.firebaseapp.com",
   projectId: "the-notepad-2e341",
   storageBucket: "the-notepad-2e341.appspot.com", // Fixed storageBucket format
-  messagingSenderId: "228673451214",
-  appId: "1:228673451214:web:d73c4a5d0df9b4a7dc8c9f"
+  messagingSenderId: "..",
+  appId: ".."
 };
 
 // Initialize Firebase
